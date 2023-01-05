7fc2be7537ea2b5987fae95c688c1876e305fbcf
SaTaN <atchuthan.siva@gmail.com>
2023-01-05 22:17:42 +0530

    Project completed!

--- src/main/java/com/practice/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/practice/springdatajpa/SpringDataJpaApplication.java
@@ -1,13 +1,31 @@
 package com.practice.springdatajpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataJpaApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student Ajay = new Student(
+                    "Ajay",
+                    "Siva",
+                    "ätchuthan.siva@gmail.com",
+                    22
+            );
+            studentRepository.save(Ajay);
+        };
     }
 
 }
